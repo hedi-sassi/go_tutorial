a039d6fb46f4f21df554138c27805358553a35c9
hedi-sassi <hedi.sassi96@gmail.com>
2020-11-17 21:33:49 +0100

    test written

---
+++ modules_tutorial/greetings/greetings_test.go
@@ -0,0 +1,31 @@
+
+//since the filename ends with "_test.go" the "go test"
+//knows that the file contains tests7
+
+//go test will execute all functions that begin with "Test"
+package greetings
+
+import (
+    "testing"
+    "regexp"
+)
+
+// TestHelloName calls greetings.Hello with a name, checking 
+// for a valid return value.
+func TestHelloName(t *testing.T) {
+    name := "Gladys"
+    want := regexp.MustCompile(`\b`+name+`\b`)
+    msg, err := Hello("Gladys")
+    if !want.MatchString(msg) || err != nil {
+        t.Fatalf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err, want)
+    }
+}
+
+// TestHelloEmpty calls greetings.Hello with an empty string, 
+// checking for an error.
+func TestHelloEmpty(t *testing.T) {
+    msg, err := Hello("")
+    if msg != "" || err == nil {
+        t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
+    }
+}
