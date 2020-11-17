dc5fa4794c37e2730f3e7747f4d4f0af64eb8a25
hedi-sassi <hedi.sassi96@gmail.com>
2020-11-17 21:09:20 +0100

    added error handling

--- modules_tutorial/hello/hello.go
+++ modules_tutorial/hello/hello.go
@@ -2,14 +2,23 @@ package main
 
 import (
     "fmt"
-
+	"log"
 	"example.com/greetings" 
 	//our own module created before... 
 	//but this link won't work if we don't modify the module
 )
 
 func main() {
+
+	log.SetPrefix("Error, greetings: ")
+	log.SetFlags(0)
+
     // Get a greeting message and print it.
-    message := greetings.Hello("Gladys")
+	message, err := greetings.Hello("Bob")
+	
+	if(err != nil){
+		log.Fatal(err)
+	}
+
     fmt.Println(message)
 }
